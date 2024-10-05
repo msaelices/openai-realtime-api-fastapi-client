96398488b00c87bd16f6dbcfdeb0dbb30a9aa660
Manuel Saelices <msaelices@gmail.com>
2024-10-05 14:21:39 +0200

    Remove some uneeded first message

--- server.py
+++ server.py
@@ -58,7 +58,6 @@ async def incoming_call(request: Request):
     print('Incoming Call:', form)
     twiml_response = f'''<?xml version='1.0' encoding='UTF-8'?>
 <Response>
-    <Say>Please wait while we connect your call to the A. I. voice assistant, powered by Twilio and the Open-A.I. Realtime API</Say>                                                              
     <Pause length='1'/>                                                                                                                                                                           
     <Say>O.K. you can start talking!</Say>
     <Connect>
